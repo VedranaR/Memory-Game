c0fd9f5a9d9ddf772501d4464f706b7a454174c2
Vedrana <vedrana_ravlic16@yahoo.com>
2018-11-22 22:57:58 +0100

    FA icon classes defined

---
+++ js/app2.js
@@ -0,0 +1,19 @@
+//Font Awesome icon classes to shuffle and add
+let objects = [
+  "html5",
+  "html5",
+  "js-square",
+  "js-square",
+  "react",
+  "react",
+  "angular",
+  "angular",
+  "css3-alt",
+  "css3-alt",
+  "sass",
+  "sass",
+  "vuejs",
+  "vuejs",
+  "gulp",
+  "gulp"
+];
